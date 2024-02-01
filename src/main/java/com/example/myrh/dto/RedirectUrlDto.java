fb366b9ae7f09b54d78fd919042fd01a91424223
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-02-01 11:40:57 +0100

    refactoring some code base to use good practice approaches also verify all endpoints

---
+++ src/main/java/com/example/myrh/dto/RedirectUrlDto.java
@@ -0,0 +1,12 @@
+package com.example.myrh.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class RedirectUrlDto {
+    private String url;
+}
